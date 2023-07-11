58f4bdbcdc1f1c18ee5ceda89137f7b76b01afc8
Negar Baharmand <negarbaharmand@outlook.com>
2023-07-11 09:36:13 +0200

    Create separate methods for each operation.

--- src/main/java/com/baharmand/Calculator.java
+++ src/main/java/com/baharmand/Calculator.java
@@ -2,14 +2,13 @@ package com.baharmand;
 
 import java.util.Scanner;
 
-
-public class App {
+public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What's your name?");
         String name = scanner.nextLine();
-        System.out.println( "Hi " + name + "! Let's calculate stuff!" );
+        System.out.println("Hi " + name + "! Let's calculate stuff!");
         boolean exit = false;
         while (!exit) {
             System.out.println("Enter the first number:");
@@ -21,7 +20,7 @@ public class App {
             } catch (Exception e) {
                 System.out.println("Invalid input! Please enter a valid number.");
                 scanner.nextLine();
-                continue; //Move to a new iteration
+                continue; // Move to a new iteration
             }
 
             System.out.println("Choose the operation:");
@@ -35,13 +34,26 @@ public class App {
             float num2 = scanner.nextFloat();
             scanner.nextLine();
 
-            float result =calculateResult(num1, num2, choose);
-            // Handling errors
-             if (result == Float.MIN_VALUE){
-                System.out.println("Operation is not valid!");
-            } else {
-                 System.out.println("The result equals to " + result);
-             }
+            float result;
+            switch (choose) {
+                case 1:
+                    result = add(num1, num2);
+                    break;
+                case 2:
+                    result = subtract(num1, num2);
+                    break;
+                case 3:
+                    result = multiply(num1, num2);
+                    break;
+                case 4:
+                    result = divide(num1, num2);
+                    break;
+                default:
+                    System.out.println("Invalid operation!");
+                    continue; // Move to a new iteration
+            }
+
+            System.out.println("The result equals to " + result);
 
             System.out.println("Do you want to perform another calculation? (y/n)");
             String choice = scanner.next();
@@ -51,28 +63,26 @@ public class App {
             }
         }
 
-
         System.out.println("Exiting the program.");
     }
-    public static float calculateResult(float num1, float num2, int choose) {
-        float result = 0.0f;
 
-        if (choose == 1) {
-            result = num1 + num2;
-        } else if (choose == 2) {
-            result = num1 - num2;
-        } else if (choose == 3) {
-            result = num1 * num2;
-        } else if (choose == 4) {
-            if (num2 == 0.0f) {
-                System.out.println("Error: Division by zero");
-                return Float.MIN_VALUE; //indicate an error condition
-            }
-            result = num1 / num2;
-        } else {
-            return Float.MIN_VALUE; //indicate an error condition
+    public static float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public static float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public static float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    public static float divide(float num1, float num2) {
+        if (num2 == 0.0f) {
+            System.out.println("Error: Division by zero");
+            return Float.MIN_VALUE; // indicate an error condition
         }
-
-        return result;
+        return num1 / num2;
     }
 }
